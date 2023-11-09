68b34a2e2ba1aa7cf6af7aae741d06cd7bbbadc8
Abdisa Dev <abdisadev@gmail.com>
2023-11-10 00:00:17 +0300

    fixed my first bug on java

---
+++ 1.10/Bug.java
@@ -0,0 +1,13 @@
+// let's fix bugs on this code
+// line 4 class name not defined so that i just called the class Bug
+// line 7 the keyword isn't valid it should be static not Static
+// line 10 we didnt call System class properly - the text we passed in to println method doesn't have its own datatype so we set it to string - at the end we must have to add semicoln 
+// line 13 we have to specify the end of the block in this case the public class Bug
+
+public class Bug
+{
+public static void main(String[] args)
+ {
+System.out.println("I want this program to compile");
+ } 
+}
---
+++ 1.10/HelloWorld_3.java
@@ -0,0 +1,8 @@
+public class HelloWorld_3 {
+    public static void main(String[] args) {
+        System.out.print("Hello World!");
+        System.out.println("Hello World! again!");
+    }
+}
+
+// tested all the example code on the book
