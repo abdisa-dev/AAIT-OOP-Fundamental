9bd2f8d899325a39ee54d02c1ca1ad0d6b12ca3c
Abdisa Dev <abdisadev@gmail.com>
2023-11-10 00:04:12 +0300

    adpted new line between address and phone number

---
+++ 1.10/NewLine.java
@@ -0,0 +1,7 @@
+public class NewLine {
+    public static void main(String[] args) {
+        System.out.println("Abdisa Alemu");        
+        System.out.println("Yeka Abado\n");
+        System.out.println("+251988242371");
+    }
+}
