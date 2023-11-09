9e0d57080a38d014832770fd923c3e2526a6d3f9
Abdisa Dev <abdisadev@gmail.com>
2023-11-10 00:02:38 +0300

    personal info

---
+++ 1.10/Me.java
@@ -0,0 +1,7 @@
+public class Me {
+    public static void main(String[] args) {
+        System.out.println("Abdisa Alemu");        
+        System.out.println("Yeka Abado");
+        System.out.println("+251988242371");
+    }
+}
